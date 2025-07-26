8c9147b511a282d51679a38c1e92e6243bfe1859
Harsha H <harsha2041889@gmail.com>
2025-07-26 10:51:27 +0530

    Initial commit

---
+++ Contacts.py
@@ -0,0 +1,11 @@
+class Contacts:
+    def __init__(self, firstname, lastname, address, city, state, zip, phone_no, email):
+        self.first = firstname
+        self.last = lastname
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.phone = phone_no
+        self.email = email
+        
