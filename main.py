8b75ab206502b7b559998729be9621fa8b02c629
Harsha H <harsha2041889@gmail.com>
2025-07-28 20:13:43 +0530

    UseCase 6: Updated

--- main.py
+++ main.py
@@ -1,39 +1,82 @@
 
 from addressBook import AddressBook
-# Step1: Print the welcome statement 
 
 print("Welcome to Address Book Program")
 
-# Step2: Creating the object of the AddressBook()
-address_book = AddressBook()
+address_book = dict()
 
-# Step3: Creating the switch statement using the below logic
 while True:
-    print(f"Choose Options",
-          f"1.Add Contact",
-          f"2.Display Contacts",
-          f"3.Edit Contact",
-          f"4.Delete Contact",
-          f"5.Exit", sep="\n")
-    
-    option = int(input("Enter the option: "))
-    
-    if option == 1:
-        #Step4: calling the add_contacts() functions in the AddressBook
-        address_book.add_contacts()
+    print("\n Choose Option: ")
+    print("1. Add a new Address Book")
+    print("2. Select an Address Book")
+    print("3. Display all Address Books ")
+    print("4. Exit")
 
-    elif option == 2:
-        # Step5: Calling the display_contacts() functions in the AddressBook
-        address_book.display_contacts()
-
-    elif option ==3:
-        # Use Case 3 edit contact is added
-        address_book.edit_contacts()
+    option_input = input("Enter Option: ")
+    option = option_input.isdigit() and int(option_input) or None
     
-    elif option == 4:
-        # Use Case 4 delete option is added
-        address_book.delete_contact()
-    else:
-        # Step6: Option to Exit the while loop
-        print("Thank you")
-        break
+    if option is None:
+        print("Enter the Integer value: ")
+        
+    else:    
+        match option:
+            case 1:
+                ab_name = input("Enter the name of the address book: ")
+                if ab_name in address_book:
+                    print("The book already exists! ")
+                
+                else:
+                    address_book[ab_name] = AddressBook()
+                    print(f"Address book with name {ab_name} is created.")
+            
+            case 2:
+                name = input("Enter the name of the address book: ")
+                
+                if name not in address_book:
+                    print(f"The Address book with the name {name} is not found! ")
+                else:
+                    select = address_book[name]
+                    
+                    while True: 
+                        print(f"\n Address Book : {name}")
+                        print("1. Add Contact")
+                        print("2. Display Contacts")
+                        print("3. Edit Contact")
+                        print("4. Delete Contact")
+                        print("5. Back to Address Book Menu")
+
+                        choice = int(input("Choose option: "))
+
+                        if choice == 1:
+                            select.add_contacts()
+
+                        elif choice == 2:
+                            select.display_contacts()
+
+                        elif choice == 3:
+                            select.edit_contacts()
+                        
+                        elif choice == 4:
+                            select.delete_contact()
+                        
+                        elif choice == 5:
+                            break
+                        else:
+                            print("Invalid Option! ")
+            
+            case 3:
+                if len(address_book) == 0:
+                    print("No Address Book Found! ")
+                else: 
+                    for book in address_book:
+                        print(book)
+            
+            case 4:
+                print("Thank You ")
+                break
+
+            case _ :
+                print("Invalid Option! ")
+
+
+            
